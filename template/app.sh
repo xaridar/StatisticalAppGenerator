a9b8a14c027b71761d9c467e6c728c11f0e7e91c
Elliot Topper <elliot.topper@nist.gov>
2024-08-07 12:27:28 -0600

    unix fixes

--- template/app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pip install -r requirements.txt
-if command -v Rscript &> /dev/null
-then
-    Rscript install.packages("shinylight", repos = "https://cloud.r-project.org")
-fi
-main.py
